8efc06a06a29d66c847a8c389330a723b35f4245
adept15n <159250613+adept15n@users.noreply.github.com>
2024-09-28 23:27:25 +0800

    Update 3-Prompt.sh
    Removed unnecessary code.

--- 3-Prompt.sh
+++ 3-Prompt.sh
@@ -9,5 +9,3 @@ cp ~/.bashrc ~/.bashrc.bak
 echo 'export PS1="-[\[$(tput sgr0)\]\[\033[38;5;10m\]\D{%Y.%m.%d}\[$(tput sgr0)\]-\[$(tput sgr0)\]\[\033[38;5;10m\]\t\[$(tput sgr0)\]]-[\[$(tput sgr0)\]\[\033[38;5;214m\]\u\[$(tput sgr0)\]@\[$(tput sgr0)\]\[\033[38;5;196m\]\h\[$(tput sgr0)\]]-\n-[\[$(tput sgr0)\]\[\033[38;5;33m\]\w\[$(tput sgr0)\]]\\$ \[$(tput sgr0)\]"' >> ~/.bashrc
 
 echo -e "\e[1;34m>>>>>>>>>>\e[0m" Done!
-
-done
