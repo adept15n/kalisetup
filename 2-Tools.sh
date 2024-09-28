e2379e33fb147c3a4142d23ec28837a929587553
adept15n <159250613+adept15n@users.noreply.github.com>
2024-09-28 23:48:12 +0800

    Updated Sublime Text

--- 2-Tools.sh
+++ 2-Tools.sh
@@ -3,8 +3,8 @@
 echo -e "\e[1;34m>>>>>>>>>>\e[0m" Getting new tools...
 
 #### Sublime Text
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-echo "deb https://download.sublimetext.com/ apt/dev/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt update
 sudo apt install sublime-text
 
