91255ca34af2791e40f37bf7692cf5e9920acb5e
adept15n <159250613+adept15n@users.noreply.github.com>
2024-10-13 18:40:16 +0800

    Delete 1-Update.sh

--- 1-Update.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-echo -e "\e[1;34m>>>>>>>>>>\e[0m" Updating package manager...
-sudo apt update -y && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y
-
-done
